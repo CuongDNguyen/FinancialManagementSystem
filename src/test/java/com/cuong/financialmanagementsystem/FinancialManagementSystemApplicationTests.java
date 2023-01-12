0f9b0edd3c5a0ad5d146380aa51c7a4cab6bce70
Cuong Nguyen <cduynguyen95@gmail.com>
2023-01-11 21:27:53 -0500

    Initial commit

---
+++ src/test/java/com/cuong/financialmanagementsystem/FinancialManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuong.financialmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
