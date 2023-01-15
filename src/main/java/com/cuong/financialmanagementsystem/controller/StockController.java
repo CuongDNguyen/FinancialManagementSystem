bfd72a79aa0bbc95c87d8307ceb59a4ef2bd8e3c
Cuong Nguyen <cduynguyen95@gmail.com>
2023-01-15 00:17:30 -0500

    Update params to use stock symbols.

--- src/main/java/com/cuong/financialmanagementsystem/controller/StockController.java
+++ src/main/java/com/cuong/financialmanagementsystem/controller/StockController.java
@@ -1,24 +1,34 @@
 package com.cuong.financialmanagementsystem.controller;
 
+import com.cuong.financialmanagementsystem.model.YahooStock;
 import com.cuong.financialmanagementsystem.service.StockImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 @Slf4j
 public class StockController {
 
-    private StockImpl stockImpl;
-
     @GetMapping(value = "/view-stocks", produces = "application/json")
-    public ResponseEntity<String> viewStocks(String accountId) {
-        stockImpl = new StockImpl();
-        stockImpl.viewStock("test");
+    public Mono viewStocks(@RequestParam String stockSymbol) {
+        try {
+            String YAHOO_URL = "https://query1.finance.yahoo.com/v11/finance/quoteSummary/" + stockSymbol + "?modules=financialData";
+            WebClient client = WebClient.create();
 
-        return ResponseEntity.status(HttpStatus.OK).body("test");
+            WebClient.ResponseSpec responseSpec = client.get()
+                    .uri(YAHOO_URL)
+                    .retrieve();
+
+            return responseSpec.bodyToMono(YahooStock.class);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return Mono.just("Exception: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/cuong/financialmanagementsystem/service/StockImpl.java
+++ src/main/java/com/cuong/financialmanagementsystem/service/StockImpl.java
@@ -1,20 +1,12 @@
 package com.cuong.financialmanagementsystem.service;
 
-import com.cuong.financialmanagementsystem.client.YahooClient;
-import com.cuong.financialmanagementsystem.model.YahooStock;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class StockImpl implements StockRepository{
 
-    private YahooClient yahooClient = new YahooClient();
+    public String viewStock(String accountId) { return null; }
 
-    public String viewStock(String accountId) {
-        yahooClient.getDataBySymbol("test");
-        return "view stock";
-    }
-
-    @Override
     public String addStock(String stockName) {
         return null;
     }
